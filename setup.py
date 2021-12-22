6594b70e2ea449a37500a1ec20b85997a236a6bf
Vane4ka <93877073+dthseemsbttr@users.noreply.github.com>
2021-12-23 01:45:10 +0300

    setup updated

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='numeric_methods_dths', 
+setup(name='dthsnumericmethods', 
       version='0.1', 
-      url='https://github.com/dthseemsbttr/Numeric-Methods-Dths', 
+      url='https://github.com/dthseemsbttr/numericmethodsdths', 
       license='MIT', 
       author='Vera Korotkova',
       author_email='verakorotkova10@mail.ru', 
-      description='Numeric methods library',  
+      description='Numeric methods library',
+      packages=find_packages(),
       long_description=open('README.md').read(),
       setup_requires=['sympy>=1.6.2', 'mpmath>=1.1.0', 'scipy>=1.5.2',
                       'numpy>=1.19.2,<1.21', 'matplotlib>=3.5.0',
